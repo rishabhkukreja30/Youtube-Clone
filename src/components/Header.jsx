0dff9bfbf058403e674767c0255e2f2be4069751
rishabhkukreja30 <rishabhkukreja30@gmail.com>
2024-03-11 23:26:37 +0530

    added header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import InputBox from "./InputBox";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="grid grid-cols-12 m-2 p-2 shadow-md">
+      <div className="flex col-span-1">
+        <img src="/menu-icon.png" alt="menu-icon" className="mx-2 h-10" />
+        <img src="/youtube-logo.png" alt="menu-icon" className="h-10" />
+      </div>
+      <InputBox />
+      <div className="col-span-1">
+        <img src="/user-logo.png" alt="user-icon" className="h-10" />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
---
+++ src/components/InputBox.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const InputBox = () => {
+  return (
+    <div className="col-span-10 flex justify-center">
+      <input
+        type="text"
+        placeholder=" Search"
+        className="border border-slate-400 rounded-l-full w-1/2 p-2"
+      />
+      <button className="border border-slate-400 p-2 rounded-r-full bg-slate-50">
+        Search
+      </button>
+    </div>
+  );
+};
+
+export default InputBox;
